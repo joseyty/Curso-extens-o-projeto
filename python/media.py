e72c6eec859f635ff64ed6374e34007392320bbb
Igor jose <lmelo7262@gmail.com>
2026-05-31 02:41:52 -0300

    update

---
+++ python/media.py
@@ -0,0 +1,18 @@
+
+Nota1=[0]
+nota2=[0]
+
+
+media= (  Nota1 + nota2)/2{
+    print( "A média é: " + media)
+}
+
+
+
+Nota1= [1,2,3,4,5,6,7,8,9,10]
+nota2= [1,2,3,4,5,6,7,8,9,10]
+
+
+media=(( sum(Nota1) + sum(nota2))/2){
+    print( "A média é: " + media)
+}
